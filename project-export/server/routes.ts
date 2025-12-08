6f5fceeb9cb488829edda1a6da714fed6039a98c
arteezy241 <arteezy241@gmail.com>
2025-12-09 01:35:34 +0800

    Fix: Import storage correctly to stop server crash

--- project-export/server/routes.ts
+++ project-export/server/routes.ts
@@ -1,6 +1,6 @@
 import type { Express } from "express";
 import { createServer, type Server } from "http";
-import { db } from "./storage";
+import { storage } from "./storage";
 import { insertCartItemSchema, insertOrderSchema } from "@shared/schema";
 import { z } from "zod";
 
